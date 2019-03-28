232e458e7fdcdad30795fef9105d20b580b2feee
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-03-28 10:11:17 +0900

    https://forum.katalon.com/t/method-code-too-large/9475/26

---
+++ Scripts/TC1/Script1553733957950.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+import groovy.text.TemplateEngine
+import internal.GlobalVariable as GlobalVariable
+
+/**
+ * This code was copied from the post https://forum.katalon.com/t/method-code-too-large/9475/27
+ * 
+ */
+'Given'
+WS.comment("Setup")
+def requestUrl = "http://demoaut.katalon.com"
+def adminToken = "foo"
+jsonTemplate = new File(".\\Include\\resources\\logUnique.json").text
+bindings = [ 'campaign' : campaign, 'event' : event ]
+
+//TemplateEngine engine = new groovy.text.SimpleTemplateEngine()
+//String reqBody = engine.createTemplate(jsonTemplate).make(bindings).toString()
+
+//TestObject request = findTestObject('events/EventsLogUnique', [('url') : url, ('body') : reqBody, ('token') : GlobalVariable.sysadminToken])
+
+'When'
+//WS.comment("Sending POST request: ${request.getRestUrl()}")
+//WS.comment("With body: ${request.getHttpBody()}")
+//responseBody = WS.sendRequest(request)
+//println (responseBody)
+
+'Then'
+//WS.comment("Check response status")
+//sCode = responseBody.statusCode
+//println (sCode)
---
+++ Test Suites/TS1.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9e9bb048-0485-41cf-ad46-bd04cc184214</testSuiteGuid>
+   <testCaseLink>
+      <guid>19c4a2ca-debb-4d51-a81a-327f9ae3a1a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>fc83bcef-6b4d-4f89-890f-17932957f638</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/WSAT Test Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>fc83bcef-6b4d-4f89-890f-17932957f638</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>campaign</value>
+         <variableId>26fa8cb2-aa06-4595-b3ef-4ba82cc557ba</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>fc83bcef-6b4d-4f89-890f-17932957f638</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>event</value>
+         <variableId>da40da05-1c1e-4da1-a0f5-a4c41cf03659</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
